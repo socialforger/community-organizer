da5da28212c39dc6bcc423a8581dd32639ef8fe7
Socialforger <socialforger@gmail.com>
2026-01-18 05:35:11 +0100

    Update comorg-module-solidarity.php

--- app/modules/comorg-solidarity/comorg-module-solidarity.php
+++ app/modules/comorg-solidarity/comorg-module-solidarity.php
@@ -1,1 +1,25 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * ComOrg – Solidarity Module
+ */
+class ComOrg_Module_Solidarity {
+
+    public static function init() {
+        self::includes();
+        self::hooks();
+    }
+
+    protected static function includes() {
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-solidarity/classes/class-comorg-solidarity-manager.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-solidarity/classes/class-comorg-solidarity-hooks.php';
+    }
+
+    protected static function hooks() {
+        ComOrg_Solidarity_Manager::init();
+        ComOrg_Solidarity_Hooks::init();
+    }
+}
+
+ComOrg_Module_Solidarity::init();
