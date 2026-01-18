7b817543afa3a66e2bb18bb2aa1ba7b296ad708b
Socialforger <socialforger@gmail.com>
2026-01-18 05:11:51 +0100

    Update comorg-integration-woocommerce.php

--- app/integrations/woocommerce/comorg-integration-woocommerce.php
+++ app/integrations/woocommerce/comorg-integration-woocommerce.php
@@ -1,1 +1,24 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * ComOrg – WooCommerce Integration
+ */
+class ComOrg_Integration_WooCommerce {
+
+    public static function init() {
+
+        if ( ! class_exists( 'WooCommerce' ) ) {
+            return;
+        }
+
+        add_action( 'init', array( __CLASS__, 'register_hooks' ) );
+    }
+
+    public static function register_hooks() {
+        // Esempio: aggiungi un campo ordine
+        // add_action( 'woocommerce_checkout_update_order_meta', ... );
+    }
+}
+
+ComOrg_Integration_WooCommerce::init();
