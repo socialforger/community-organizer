3d80329645dd7a0b36f9e2fdf6061276b4d12e3d
Socialforger <socialforger@gmail.com>
2026-01-18 05:06:10 +0100

    Update class-comorg-component.php

--- app/core/classes/class-comorg-component.php
+++ app/core/classes/class-comorg-component.php
@@ -1,1 +1,21 @@
 
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Base Component (simile a BP_Component)
+ */
+abstract class ComOrg_Component {
+
+    public $id = '';
+    public $name = '';
+
+    public function __construct( $id, $name ) {
+        $this->id   = $id;
+        $this->name = $name;
+
+        $this->setup_hooks();
+    }
+
+    abstract protected function setup_hooks();
+}
