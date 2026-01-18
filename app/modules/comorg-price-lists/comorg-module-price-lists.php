cc91a0fa522358f5bfe08a10f33c22bf1491c78d
Socialforger <socialforger@gmail.com>
2026-01-18 05:30:46 +0100

    Update comorg-module-price-lists.php

--- app/modules/comorg-price-lists/comorg-module-price-lists.php
+++ app/modules/comorg-price-lists/comorg-module-price-lists.php
@@ -1,1 +1,27 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * ComOrg – Price Lists Module
+ */
+class ComOrg_Module_Price_Lists {
+
+    public static function init() {
+        self::includes();
+        self::hooks();
+    }
+
+    protected static function includes() {
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-price-lists/classes/class-comorg-price-parser.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-price-lists/classes/class-comorg-price-sync.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-price-lists/classes/class-comorg-price-hooks.php';
+    }
+
+    protected static function hooks() {
+        ComOrg_Price_Parser::init();
+        ComOrg_Price_Sync::init();
+        ComOrg_Price_Hooks::init();
+    }
+}
+
+ComOrg_Module_Price_Lists::init();
