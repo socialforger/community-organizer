c27b5fa85d065821df75017ae2ae4fa32d719291
Socialforger <socialforger@gmail.com>
2026-01-18 05:06:44 +0100

    Update class-comorg-install.php

--- app/core/classes/class-comorg-install.php
+++ app/core/classes/class-comorg-install.php
@@ -1,1 +1,16 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * Installazione e tabelle DB
+ */
+class ComOrg_Install {
+
+    public static function activate() {
+        // Qui creerai le tabelle
+    }
+
+    public static function deactivate() {
+        // Cleanup se necessario
+    }
+}
