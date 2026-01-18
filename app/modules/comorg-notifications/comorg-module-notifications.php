f7f7f5be92080f72c83a993d7a0ca7a62bd38b28
Socialforger <socialforger@gmail.com>
2026-01-18 05:38:09 +0100

    Update comorg-module-notifications.php

--- app/modules/comorg-notifications/comorg-module-notifications.php
+++ app/modules/comorg-notifications/comorg-module-notifications.php
@@ -1,1 +1,27 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * ComOrg – Notifications Module
+ */
+class ComOrg_Module_Notifications {
+
+    public static function init() {
+        self::includes();
+        self::hooks();
+    }
+
+    protected static function includes() {
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-notifications/classes/class-comorg-notifications-manager.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-notifications/classes/class-comorg-notifications-onesignal.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-notifications/classes/class-comorg-notifications-hooks.php';
+    }
+
+    protected static function hooks() {
+        ComOrg_Notifications_Manager::init();
+        ComOrg_Notifications_OneSignal::init();
+        ComOrg_Notifications_Hooks::init();
+    }
+}
+
+ComOrg_Module_Notifications::init();
