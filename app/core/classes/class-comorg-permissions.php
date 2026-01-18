114a61dd30c0ea06d9112bd7d2079dfb4906cc2c
Socialforger <socialforger@gmail.com>
2026-01-18 05:07:15 +0100

    Update class-comorg-permissions.php

--- app/core/classes/class-comorg-permissions.php
+++ app/core/classes/class-comorg-permissions.php
@@ -1,1 +1,12 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * Ruoli e permessi ComOrg
+ */
+class ComOrg_Permissions {
+
+    public static function register_roles() {
+        // Aggiungi ruoli ComOrg
+    }
+}
