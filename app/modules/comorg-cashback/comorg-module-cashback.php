c1d813bce99dcb3c77d5722e626cee25bdeee6ad
Socialforger <socialforger@gmail.com>
2026-01-18 05:33:23 +0100

    Update comorg-module-cashback.php

--- app/modules/comorg-cashback/comorg-module-cashback.php
+++ app/modules/comorg-cashback/comorg-module-cashback.php
@@ -1,1 +1,27 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * ComOrg – Cashback Module
+ */
+class ComOrg_Module_Cashback {
+
+    public static function init() {
+        self::includes();
+        self::hooks();
+    }
+
+    protected static function includes() {
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-cashback/classes/class-comorg-cashback-db.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-cashback/classes/class-comorg-cashback-manager.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-cashback/classes/class-comorg-cashback-hooks.php';
+    }
+
+    protected static function hooks() {
+        ComOrg_Cashback_DB::init();
+        ComOrg_Cashback_Manager::init();
+        ComOrg_Cashback_Hooks::init();
+    }
+}
+
+ComOrg_Module_Cashback::init();
