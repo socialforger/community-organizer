73c099e013d029c32df6513ce902d4e1de40dd57
Socialforger <socialforger@gmail.com>
2026-01-18 05:01:58 +0100

    Update class-comorg-admin-wizard.php

--- app/admin/classes/class-comorg-admin-wizard.php
+++ app/admin/classes/class-comorg-admin-wizard.php
@@ -1,1 +1,12 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * ComOrg Setup Wizard
+ */
+class ComOrg_Admin_Wizard {
+
+    public static function render() {
+        include COMORG_PLUGIN_DIR . 'app/admin/screens/wizard.php';
+    }
+}
