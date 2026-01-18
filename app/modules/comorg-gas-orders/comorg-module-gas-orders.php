b9909277db70084ed3706b7d3ed0b575f37af83d
Socialforger <socialforger@gmail.com>
2026-01-18 05:20:59 +0100

    Update comorg-module-gas-orders.php

--- app/modules/comorg-gas-orders/comorg-module-gas-orders.php
+++ app/modules/comorg-gas-orders/comorg-module-gas-orders.php
@@ -1,1 +1,29 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * ComOrg – GAS Orders Module
+ */
+class ComOrg_Module_GAS_Orders {
+
+    public static function init() {
+        self::includes();
+        self::hooks();
+    }
+
+    protected static function includes() {
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-gas-orders/classes/class-comorg-gas-cpt.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-gas-orders/classes/class-comorg-gas-manager.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-gas-orders/classes/class-comorg-gas-cron.php';
+        require_once COMORG_PLUGIN_DIR . 'app/modules/comorg-gas-orders/classes/class-comorg-gas-hooks.php';
+    }
+
+    protected static function hooks() {
+        ComOrg_GAS_CPT::init();
+        ComOrg_GAS_Manager::init();
+        ComOrg_GAS_Cron::init();
+        ComOrg_GAS_Hooks::init();
+    }
+}
+
+ComOrg_Module_GAS_Orders::init();
