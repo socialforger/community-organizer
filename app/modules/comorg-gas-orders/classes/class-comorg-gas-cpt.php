f180fc5a21f808811ee9deb19edacce5da3e4347
Socialforger <socialforger@gmail.com>
2026-01-18 05:22:06 +0100

    Update class-comorg-gas-cpt.php

--- app/modules/comorg-gas-orders/classes/class-comorg-gas-cpt.php
+++ app/modules/comorg-gas-orders/classes/class-comorg-gas-cpt.php
@@ -1,1 +1,13 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+class ComOrg_GAS_CPT {
+
+    public static function init() {
+        add_action( 'init', array( __CLASS__, 'register_cpt' ) );
+    }
+
+    public static function register_cpt() {
+        // register_post_type( 'comorg_gas_order', ... );
+    }
+}
