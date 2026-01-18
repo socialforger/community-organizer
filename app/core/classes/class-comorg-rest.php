603ce7d8546a5d04ec4d0cbb5aedf4559ce90a06
Socialforger <socialforger@gmail.com>
2026-01-18 05:07:44 +0100

    Update class-comorg-rest.php

--- app/core/classes/class-comorg-rest.php
+++ app/core/classes/class-comorg-rest.php
@@ -1,1 +1,12 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * REST API ComOrg
+ */
+class ComOrg_REST {
+
+    public static function register_routes() {
+        // Registra namespace comorg/v1
+    }
+}
