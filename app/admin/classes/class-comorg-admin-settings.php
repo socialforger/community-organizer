83be6b8434441296d256bec38cf64d5b7c3a7b1c
Socialforger <socialforger@gmail.com>
2026-01-18 05:01:20 +0100

    Update class-comorg-admin-settings.php

--- app/admin/classes/class-comorg-admin-settings.php
+++ app/admin/classes/class-comorg-admin-settings.php
@@ -1,1 +1,24 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * ComOrg Settings Page
+ */
+class ComOrg_Admin_Settings {
+
+    public static function render() {
+        ?>
+        <div class="wrap comorg-admin-wrap">
+            <h1><?php _e( 'ComOrg Settings', 'comorg' ); ?></h1>
+
+            <form method="post" action="options.php">
+                <?php
+                settings_fields( 'comorg_settings' );
+                do_settings_sections( 'comorg_settings' );
+                submit_button();
+                ?>
+            </form>
+        </div>
+        <?php
+    }
+}
