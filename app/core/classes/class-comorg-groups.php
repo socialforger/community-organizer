5237237160cd8d6023c4de1316c0af9e863ce66c
Socialforger <socialforger@gmail.com>
2026-01-18 05:08:23 +0100

    Update class-comorg-groups.php

--- app/core/classes/class-comorg-groups.php
+++ app/core/classes/class-comorg-groups.php
@@ -1,1 +1,12 @@
+<?php
+defined( 'ABSPATH' ) || exit;
 
+/**
+ * Gestione gruppi territoriali e tematici
+ */
+class ComOrg_Groups {
+
+    public static function init() {
+        // Hook BuddyBoss Groups
+    }
+}
